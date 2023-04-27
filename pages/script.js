19e55d63f9ef0dfd91707220fa4161c2be8c8900
Angelestm <angelestm92@gmail.com>
2023-04-26 23:10:35 -0500

    avances de sprint 5

--- pages/script.js
+++ pages/script.js
@@ -1,10 +1,10 @@
 const editButtonElement = document.querySelector(".profile__edit-button");
-const modalElement = document.querySelector(".pop-up");
-const modalCloseButtonElement = document.querySelector(".pop-up__close-button");
-const modalFormElement = document.querySelector(".form");
+const popUpOneElement = document.querySelector("#pop-up1");
+const modalCloseButtonElement = document.querySelector("#close-button1");
+const modalFormElement = document.querySelector("#form1");
 
 function handleDisplayModal() {
-  modalElement.classList.toggle("popup_opened");
+  popUpOneElement.classList.toggle("popup_opened");
 }
 
 function handleProfileFormSubmit(event) {
@@ -26,3 +26,46 @@ function handleProfileFormSubmit(event) {
 editButtonElement.addEventListener('click', handleDisplayModal);
 modalCloseButtonElement.addEventListener('click', handleDisplayModal);
 modalFormElement.addEventListener('submit', handleProfileFormSubmit);
+
+
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg"
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
+  }
+];
+
+
+
+const addButton = document.querySelector(".profile__add-button-container");
+const popUpTwo = document.querySelector("#pop-up2");
+const closeButton = document.querySelector("#close-button2");
+const FormTwo = document.querySelector("#form2");
+const deleteButton = document.querySelector(".element__delete-button");
+
+function handleDisplayModal2() {
+  popUpTwo.classList.toggle("popup_opened");
+}
+
+addButton.addEventListener('click', handleDisplayModal2);
+closeButton.addEventListener('click', handleDisplayModal2);
